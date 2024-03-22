c294a2d8485fb89ce9a61897aed6f37def3a1604
Akhilesh Pal <palakhilesh466@gmail.com>
2024-03-22 16:19:08 +0530

    use of require

---
+++ main.js
@@ -0,0 +1,7 @@
+// const math = require("./math")
+// console("addition : ", math.add(2,4))
+
+const {add,sub} = require("./math");
+console("addition : ", add(2,4))
+console("substraction : ", sub(6,3));
+
