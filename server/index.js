5600c57d90515ab6a9d42c2be4c71f4f9392ddba
Akhilesh Pal <palakhilesh466@gmail.com>
2024-03-25 01:00:51 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -4,13 +4,14 @@ const fs = require("fs");
 
 //req contain meta data of who is requesting.
 const myServer = http.createServer((req,res)=>{
- const log = `${Date.now()} : ${req.url} New Req Recieved\n`
+ const log = `${Date.now()} :${req.method} ${req.url} New Req Recieved\n`
 fs.appendFile("log.txt",log,(err,data)=>{
 switch(req.url){
     case "/":
         res.end("HomePage");
         break;
-    case "/about":    res.end("i am akhilesh pal")
+    case "/about":
+        res.end("i am akhilesh pal")
     break;
     default:
         res.end("404 not found")
