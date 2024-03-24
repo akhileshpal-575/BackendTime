63e7ccc12e2298d964de165dba40895517d80636
Akhilesh Pal <palakhilesh466@gmail.com>
2024-03-24 11:25:18 +0530

    server create async fs

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
 //http built in module
 const http = require("http");
+const fs = require("fs");
 
 //req contain meta data of who is requesting.
 const myServer = http.createServer((req,res)=>{
-console.log("New Req Recieved")
-res.end("hello from server")
+ const log = `${Date.now()} : ${req.url} New Req Recieved\n`
+fs.appendFile("log.txt",log,(err,data)=>{
+switch(req.url){
+    case "/":
+        res.end("HomePage");
+        break;
+    case "/about":    res.end("i am akhilesh pal")
+    break;
+    default:
+        res.end("404 not found")
+
+
+
+}
+})
 });
- 
 myServer.listen(8000,()=>console.log("Server Started")) 
