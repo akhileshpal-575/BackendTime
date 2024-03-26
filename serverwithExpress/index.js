f7f0e9dcea49182e852e6036d1b9b579e0140ec8
Akhilesh Pal <palakhilesh466@gmail.com>
2024-03-26 16:16:04 +0530

    exress start

---
+++ serverwithExpress/index.js
@@ -0,0 +1,12 @@
+const http = require("http")
+const express = require("express")
+const app = express()
+
+app.get("/",(req,res)=>{
+    return res.send("hello from the server")
+})
+
+app.get("/about",(req,res)=>{
+    return res.send("i'm akhilesh")
+})
+app.listen(8000,()=>console.log("server started"))
