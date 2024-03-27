edab4efc266b49ec03dc2bc7c2ea7b7e8cfdcc66
Akhilesh Pal <palakhilesh466@gmail.com>
2024-03-27 15:21:00 +0530

    get operation performed for all users and for getting single user through id

--- project-01/index.js
+++ project-01/index.js
@@ -1,8 +1,16 @@
 const express = require("express")
+const users = require("./MOCK_DATA.json")
 
 const app = express();
 const PORT = 8000;
 
-//Routes
-
+//Rest Api's
+app.get("/api/users",(req,res)=>{
+    return res.json(users);
+})
+app.get('/api/users/:id',(req,res)=>{
+const id = Number(req.params.id);
+const user = users.find((user)=> user.id === id);
+return res.json(user)
+})
 app.listen(PORT,()=> console.log(`server started at PORT 8000`))
