4d5c056dc84249326e3aeaedbf3b8feaaaa54caa
Akhilesh Pal <palakhilesh466@gmail.com>
2024-03-28 08:20:05 +0530

    post operation perform and new data added into json file

--- project-01/index.js
+++ project-01/index.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const users = require("./MOCK_DATA.json")
 
+const fs = require("fs")
+
 const app = express();
 const PORT = 8000;
 
@@ -8,9 +10,43 @@ const PORT = 8000;
 app.get("/api/users",(req,res)=>{
     return res.json(users);
 })
-app.get('/api/users/:id',(req,res)=>{
-const id = Number(req.params.id);
-const user = users.find((user)=> user.id === id);
-return res.json(user)
+
+
+app.use(express.urlencoded({extended:false}))
+
+// app.get('/api/users/:id',(req,res)=>{
+// const id = Number(req.params.id);
+// const user = users.find((user)=> user.id === id);
+// return res.json(user)
+// })
+// app.patch('/api/users/:id',(req, res)=>{
+//     // Todo : Edit user 
+//     return res.json({status:"panding"})
+// })
+// app.delete('/api/users/:id',(req, res)=>{
+//     // Todo : Delete user
+//     return res.json({status:"panding"})
+// })
+
+//Because all these above methods have same route so merge all
+
+app.route('/api/users/:id').get((req,res)=>{
+    const id = Number(req.params.id)
+    const user = users.find((user)=> user.id === id)
+    return res.json(user)
+}).patch((req,res)=>{
+    // Todo: Edit user
+    return res.json({status:"pending"})
+}).delete((req,res)=>{
+    // Todo: delete user
+    return res.json({status:'pending'})
+})
+app.post('/api/users/',(req,res)=>{
+    //Todo: create new user
+    const body = req.body
+    users.push({...body,id:users.length+1})
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err,data)=>{
+        return res.json({status:'succes',id:users.length})        
+    })
 })
 app.listen(PORT,()=> console.log(`server started at PORT 8000`))
